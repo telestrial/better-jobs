d7eb0c775d3257d42d2821431eb5e0b49e748d36
John Williams <telestrial@gmail.com>
2024-05-31 12:49:51 -0400

    add documentation for findLinkedJobID

--- src/helpers/findLinkedInJobID.ts
+++ src/helpers/findLinkedInJobID.ts
@@ -1,3 +1,9 @@
+/**
+ *
+ * @param link - a string of the linkedin URL pointer URL to the job.
+ * @returns ID - a string representing a unique job
+ */
+
 export default function findLinkedInJobID(link: string) {
   if (!link) throw new Error('No string provided to findLInkedInJobID');
 
